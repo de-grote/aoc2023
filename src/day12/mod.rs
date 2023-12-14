559a4af1bf907208e3c2c59bdb67218828dd2ffc
de-grote <chris.groot2004@gmail.com>
2023-12-14 05:58:48 +0100

    refactor: :zap: improve day 12 part 2 (it is not enough)

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -12,7 +12,7 @@ pub fn part1(input: &str) -> Answer {
 
     let result = i
         .into_iter()
-        .map(|(tiles, numbers)| combinations(&tiles, numbers))
+        .map(|(tiles, numbers)| combinations(tiles, numbers))
         .sum::<u64>();
 
     Ok(result.to_string())
@@ -30,20 +30,31 @@ pub fn part2(input: &str) -> Answer {
                 .flatten()
                 .collect_vec();
             let numbers = numbers.repeat(5);
-            combinations(&tiles, numbers)
+            combinations(tiles, numbers)
         })
         .sum::<u64>();
 
     Ok(result.to_string())
 }
 
-fn combinations(tiles: &[Tile], numbers: Vec<u32>) -> u64 {
-    let groups = tiles
+fn combinations(tiles: Vec<Tile>, numbers: Vec<u32>) -> u64 {
+    // replace all empty space with 1 space
+    let mut groups = tiles
         .split_inclusive(|&v| v == Tile::Empty)
         .filter(|x| !x.iter().all(|&t| t == Tile::Empty))
         .flatten()
         .copied()
         .collect_vec();
+
+    if groups.last().is_some_and(|&t| t == Tile::Empty) {
+        groups.pop();
+    }
+
+    optimise_right(&mut groups, &numbers);
+
+    optimise_left(&mut groups, &numbers);
+
+    // dbg!(&groups);
 
     let split_groups = groups
         .split(|&x| x == Tile::Unknown)
@@ -59,19 +70,19 @@ fn combinations(tiles: &[Tile], numbers: Vec<u32>) -> u64 {
 
     let mut res = 0;
 
-    let a = groups.iter().filter(|&&x| x == Tile::Unknown).count() as u32;
+    let unknowns_left = groups.iter().filter(|&&x| x == Tile::Unknown).count();
 
-    for i in 0..2u64.pow(a) {
-        if i.count_ones() != sum {
-            continue;
-        }
-        let a2 = (0..a).map(|b| match ((i >> b) & 1) == 0 {
+    // we want to reduce unknowns_left and sum as much as possible for the best performence
+    // println!("{} | {}", unknowns_left, sum);
+
+    for i in (0..2u64.pow(unknowns_left as u32)).filter(|&i| i.count_ones() == sum) {
+        let a2 = (0..unknowns_left).map(|b| match ((i >> b) & 1) == 0 {
             true => Tile::Empty,
             false => Tile::Filled,
         });
         let r = split_groups
-            .clone()
-            .into_iter()
+            .iter()
+            .cloned()
             .interleave(a2.map(|x| vec![x]))
             .flatten();
         // dbg!(r.clone().collect_vec());
@@ -80,6 +91,134 @@ fn combinations(tiles: &[Tile], numbers: Vec<u32>) -> u64 {
         }
     }
     res
+}
+
+fn optimise_right(groups: &mut [Tile], numbers: &[u32]) -> Option<std::convert::Infallible> {
+    let mut b = true;
+    let mut current_number = numbers.len() - 1;
+    let mut l = groups.len() - 1;
+    'outer: while b {
+        b = false;
+        let mut first = false;
+        let current = numbers[current_number] as usize;
+        for i in 0..current {
+            let tile = groups.get_mut(l - i)?;
+            if *tile == Tile::Filled {
+                b = true;
+                if i == 0 {
+                    first = true;
+                }
+            } else if *tile == Tile::Empty {
+                groups[l - i..=l].fill(Tile::Empty);
+                l -= i + 1;
+                b = true;
+                continue 'outer;
+            }
+            if b {
+                *tile = Tile::Filled;
+            }
+        }
+        if first {
+            if let Some(tile) = groups.get_mut(l.checked_sub(current)?) {
+                *tile = Tile::Empty;
+            }
+            l = l.checked_sub(current + 1)?;
+            current_number = current_number.checked_sub(1)?;
+        } else if b {
+            let s = l.checked_sub(current)?;
+            let slice = &groups[..=s];
+
+            let nearest_empty = slice
+                .iter()
+                .rev()
+                .find_position(|&&t| t == Tile::Empty)
+                .map(|x| x.0)
+                .unwrap_or(slice.len());
+            let slice = &mut groups[s.checked_sub(nearest_empty)? + 1..=l];
+            let len = slice.len() as i32;
+            let overlap = current as i32 * 2 - len;
+            if overlap <= 0 {
+                break;
+            }
+            let inv_overlap = ((len - overlap) / 2) as usize;
+            slice[inv_overlap..len as usize - inv_overlap].fill(Tile::Filled);
+
+            l = l.checked_sub(current + nearest_empty + 1)?;
+            current_number = current_number.checked_sub(1)?;
+
+            if inv_overlap as u32 > numbers[current_number] {
+                break;
+            }
+        }
+    }
+    None
+}
+
+fn optimise_left(groups: &mut [Tile], numbers: &[u32]) -> Option<std::convert::Infallible> {
+    let mut b = true;
+    let mut current_number = 0;
+    let mut l = 0;
+    'outer: while b {
+        b = false;
+        let mut first = false;
+        let current = numbers[current_number] as usize;
+        for i in 0..current {
+            let tile = groups.get_mut(l + i)?;
+            if *tile == Tile::Filled {
+                b = true;
+                if i == 0 {
+                    first = true;
+                }
+            } else if *tile == Tile::Empty {
+                groups[l..=l + i].fill(Tile::Empty);
+                l += i + 1;
+                b = true;
+                continue 'outer;
+            }
+            if b {
+                *tile = Tile::Filled;
+            }
+        }
+        if first {
+            if let Some(tile) = groups.get_mut(l + current) {
+                *tile = Tile::Empty;
+            }
+            l += current + 1;
+            current_number += 1;
+            if current_number == numbers.len() || l >= groups.len() {
+                break;
+            }
+        } else if b {
+            if l + current >= groups.len() {
+                break;
+            }
+            let slice = &groups[l + current..];
+
+            let nearest_empty = slice
+                .iter()
+                .find_position(|&&t| t == Tile::Empty)
+                .map(|x| x.0)
+                .unwrap_or(slice.len());
+            let slice = &mut groups[l..l + current + nearest_empty];
+            let len = slice.len() as i32;
+            let overlap = current as i32 * 2 - len;
+            if overlap <= 0 {
+                break;
+            }
+            let inv_overlap = ((len - overlap) / 2) as usize;
+            slice[inv_overlap..len as usize - inv_overlap].fill(Tile::Filled);
+
+            l += current + nearest_empty + 1;
+            current_number += 1;
+            if l >= groups.len() || current_number == numbers.len() {
+                break;
+            }
+            if inv_overlap as u32 > numbers[current_number] {
+                break;
+            }
+        }
+    }
+    None
 }
 
 fn valid_combination(mut it: impl Iterator<Item = Tile>, numbers: &[u32]) -> bool {
